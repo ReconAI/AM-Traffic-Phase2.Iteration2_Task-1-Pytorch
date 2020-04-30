0679c839f3e79bb25db12849fd874bbb94c5940d
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2020-04-30 14:42:11 +0200

    Update predict_trt.py

--- predict_trt.py
+++ predict_trt.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Predict the trt model on the
-Jetson Nano and get fps
+Prediction using the trt model on the
+Jetson Nano and fps computation
 """
 #import the necessary packages
 from __future__ import print_function
